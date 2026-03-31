4df467d87c12f050457442ff445d0a3a4c02ffb3
Arthur Ruiz <23.01445-8@maua.br>
2026-03-31 16:11:24 -0300

    learn(kotlin): tipos basicos de dados e conversao

--- Aula02/Kotlin_03.kt
+++ Aula02/Kotlin_03.kt
@@ -3,11 +3,24 @@ fun main(args: Array<String>) {
     var a = 10;
     var b = 2;
     var x: Int
-    
+
     while(a % b != 0){
         x = a%b
         a = b;
         b = x
     }
     println(b)
+
+    /*Tipos primitivos (básicos):
+        Inteiro (Int)
+        Long (Long)
+        Byte (Byte)
+        Curto (Short)
+        Ponto Flutuante (Float)
+        Duplo (Double) 
+    */
+    //conversão de um tipo numérico para outro
+    val intVal = 10
+    val doubleval = intVal.toDouble()       //converte int para double
+    
 }
