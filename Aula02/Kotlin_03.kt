e7c08e45a6f9d6711841ab3b5b88676eab97c8a4
Arthur Ruiz <23.01445-8@maua.br>
2026-03-31 16:35:49 -0300

    learn(kotlin): estudo de Strings

--- Aula02/Kotlin_03.kt
+++ Aula02/Kotlin_03.kt
@@ -1,15 +1,15 @@
 fun main(args: Array<String>) {
     //atribuições em kotlin não retornam valor
-    var a = 10;
-    var b = 2;
-    var x: Int
+    // var a = 10;
+    // var b = 2;
+    // var x: Int
 
-    while(a % b != 0){
-        x = a%b
-        a = b;
-        b = x
-    }
-    println(b)
+    // while(a % b != 0){
+    //     x = a%b
+    //     a = b;
+    //     b = x
+    // }
+    // println(b)
 
     /*Tipos primitivos (básicos):
         Inteiro (Int)
@@ -20,27 +20,42 @@ fun main(args: Array<String>) {
         Duplo (Double) 
     */
     //conversão de um tipo numérico para outro
-    val intVal = 10
-    val doubleval = intVal.toDouble()       //converte int para double
+    // val intVal = 10
+    // val doubleval = intVal.toDouble()       //converte int para double
 
-    //Arrays
-    val names = arrayOf("jim", "John", "Jenny")     //cria um array
-    println(names.contentToString())                //exibe o array
+    // //Arrays
+    // val names = arrayOf("jim", "John", "Jenny")     //cria um array
+    // println(names.contentToString())                //exibe o array
 
-    //cria dois arrays numéricos
-    val numbers1 = arrayOf(1,2,3,4,5)
-    val numbers2 = arrayOf(10,20,30,40,50,60,70);
+    // //cria dois arrays numéricos
+    // val numbers1 = arrayOf(1,2,3,4,5)
+    // val numbers2 = arrayOf(10,20,30,40,50,60,70);
 
-    //somador 
-    var r=0;
-    //percorre os arrays
-    var i=0;
+    // //somador 
+    // var r=0;
+    // //percorre os arrays
+    // var i=0;
     
-    while(i < numbers1.size && i< numbers2.size){
-        if(i%2==0){
-            r+= numbers1[i] + numbers2[i]
-        }
-        i++;
-    }
-    println(r)
+    // while(i < numbers1.size && i< numbers2.size){
+    //     if(i%2==0){
+    //         r+= numbers1[i] + numbers2[i]
+    //     }
+    //     i++;
+    // }
+    // println(r)
+
+    //Strings
+    val f = "João"
+    val u = "Silva";
+    val n = "$f $u"
+
+    val g = "Olá, $n! Seja bem-vindo!";
+    val a = g[g.length-1]
+    val b = if(g.startsWith("Olá")) 2 else 3
+    val l = g[g.length -2]
+    val m = u.uppercase().substring(1,2)
+    val r = u.substring(0,2)
+
+    val x = if(n.startsWith("SI")) 4 else 5;
+    println("$a$b$l$m$r$x")
 }   
