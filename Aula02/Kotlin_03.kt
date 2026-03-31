9536fa896f290fc68a56fc233a31565ee7da0f07
Arthur Ruiz <23.01445-8@maua.br>
2026-03-31 16:21:13 -0300

    learn(kotlin): estudo de arrays

--- Aula02/Kotlin_03.kt
+++ Aula02/Kotlin_03.kt
@@ -22,5 +22,25 @@ fun main(args: Array<String>) {
     //conversão de um tipo numérico para outro
     val intVal = 10
     val doubleval = intVal.toDouble()       //converte int para double
+
+    //Arrays
+    val names = arrayOf("jim", "John", "Jenny")     //cria um array
+    println(names.contentToString())                //exibe o array
+
+    //cria dois arrays numéricos
+    val numbers1 = arrayOf(1,2,3,4,5)
+    val numbers2 = arrayOf(10,20,30,40,50,60,70);
+
+    //somador 
+    var r=0;
+    //percorre os arrays
+    var i=0;
     
-}
+    while(i < numbers1.size && i< numbers2.size){
+        if(i%2==0){
+            r+= numbers1[i] + numbers2[i]
+        }
+        i++;
+    }
+    println(r)
+}   
