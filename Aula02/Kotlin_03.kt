745d1d48180ed900a60ea63bd48426603cd477b6
Arthur Ruiz <23.01445-8@maua.br>
2026-03-31 15:43:37 -0300

    learn(kotlin): implementa while sem atribuicao retornar valor

--- Aula02/Kotlin_03.kt
+++ Aula02/Kotlin_03.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-    println("hello")
+    //atribuições em kotlin não retornam valor
+    var a = 10;
+    var b = 2;
+    var x: Int
+    
+    while(a % b != 0){
+        x = a%b
+        a = b;
+        b = x
+    }
+    println(b)
 }
