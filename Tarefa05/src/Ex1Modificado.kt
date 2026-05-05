8916c8e455cd48af5b6a845ac95aa024e6b0494e
Arthur Ruiz <23.01445-8@maua.br>
2026-05-05 15:21:32 -0300

    feat(kotlin): implementa ex1 modificado

---
+++ Tarefa05/src/Ex1Modificado.kt
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+fun saudacao(nome: String?, idade: Int?): String{
+    if(idade in 18..65){
+        return ("Olá, "+nome+" ! "+"Você tem "+idade+" anos! Você pode doar sangue")
+    }
+    return ("Olá, "+nome+" ! "+"Você tem "+idade+" anos! Você não pode doar sangue")
+}
+fun main() {
+    val nome: String?
+    val idade: Int?
+
+    println("Entre com seu nome: ")
+    nome = readLine()
+
+    println("Entre com sua idade: ")
+    idade = readLine()?.toIntOrNull()
+
+
+    val retorno = saudacao(nome, idade)
+    println(retorno)
+}
