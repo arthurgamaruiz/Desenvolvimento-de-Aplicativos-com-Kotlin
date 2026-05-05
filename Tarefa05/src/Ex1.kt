89b50c8ea70b6fcbdaf4f5528dc2681efdc1b252
Arthur Ruiz <23.01445-8@maua.br>
2026-05-05 15:11:46 -0300

    learn(kotlin): estudo sobre funcoes. Implementa ex1

--- Tarefa05/src/Ex1.kt
+++ Tarefa05/src/Ex1.kt
@@ -1,5 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
 
+fun saudacao(nome: String?, idade: Int): String{
+    return ("Olá, "+nome+" ! "+"Você tem "+idade+" anos!")
+}
+fun main() {
+    val nome: String?
+    val idade: Int?
+
+    println("Entre com seu nome: ")
+    nome = readLine()
+
+    println("Entre com sua idade: ")
+    idade = readLine()?.toIntOrNull()
+
+    val retorno = saudacao(nome, idade!!)
+    println(retorno)
 }
